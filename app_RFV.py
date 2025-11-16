44f1cfa68e7e87ccfdfaa25676657a774dbbe345
aquilabezerra <aquilabezerra150@gmail.com>
2025-11-16 20:02:06 -0300

    Refactor app_RFV.py to improve structure and imports

--- app_RFV.py
+++ app_RFV.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
+import xlsxwriter
 
 from datetime import datetime
 from PIL import Image
@@ -211,6 +212,7 @@ def main():
 if __name__ == '__main__':
     main()
     
+
 
 
 
